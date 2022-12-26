a5d65652c595dbd388d651f8b4033dc3b3793169
Filip Lange <filip.lange@gmail.com>
2022-12-26 20:29:48 +0100

    Remove pytest warning

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ from pytest import MonkeyPatch
 
 @pytest.fixture(autouse=True)
 def mock_devjournal_dir(monkeypatch: MonkeyPatch, tmp_path):
-    monkeypatch.setenv("DEVJOURNAL_DIR", tmp_path)
+    monkeypatch.setenv("DEVJOURNAL_DIR", str(tmp_path))
     return tmp_path
